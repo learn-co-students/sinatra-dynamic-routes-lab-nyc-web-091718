28245b048701bd2aec9a3783c9ec8ab01027cd96
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-10-09 11:56:34 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,55 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse!}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number]
+      "#{(@number.to_i)**2}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
+    phrases = ''
+    #binding.pry
+    (@number.to_i).times do
+    phrases += "#{@phrase}\n"
+    end
+    phrases
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    arr = []
+    arr << @word1 = params[:word1]
+    arr << @word2 = params[:word2]
+    arr << @word3 = params[:word3]
+    arr << @word4 = params[:word4]
+    arr << @word5 = params[:word5]
+
+    sentence = arr.join(" ")
+    sentence += "."
+  end
+
+  get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+
+    answer = 'Unable to perform this operation'
+
+    case params[:operation]
+    when 'add'
+      answer = (number1 + number2).to_s
+    when 'subtract'
+      answer = (number1 - number2).to_s
+    when 'multiply'
+      answer = (number1 * number2).to_s
+    when 'divide'
+      answer = (number1 / number2).to_s
+    end
+  end
 
 end
